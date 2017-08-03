c9d9dd7c48bb4d24588369eca146ded31c35ae44
Craig Newton <craig.newton@connection-telecom.com>
2017-08-03 16:24:12 +0200

    Added initial project and basic CRUD operations.

---
+++ todos.js
@@ -0,0 +1,57 @@
+Todos = new Mongo.Collection('todos');
+
+if (Meteor.isClient) {
+  // Template Helpers
+  Template.main.helpers({
+    todos: function() {
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.main.events({
+    "submit .new-todo": function(event) {
+      var text = event.target.text.value;
+
+      Meteor.call('addTodo', text);
+
+      // Clear Form
+      event.target.text.value = '';
+
+      // Prevent submit
+      return false;
+    },
+    "click .toggle-checked": function() {
+      Meteor.call('setChecked', this._id, !this.checked);
+    },
+    "click .delete-todo": function() {
+      if (confirm('Are You Sure?')) {
+        Meteor.call('deleteTodo', this._id);
+      }
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+}
+
+// Meteor Methods
+Meteor.methods({
+  addTodo: function(text) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId) {
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked) {
+    Todos.update(todoId, {$set: {checked: setChecked}});
+  }
+});
