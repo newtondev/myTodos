59ba19183174ceee751d6ea1b555ef16809ad3a6
Craig Newton <craig.newton@connection-telecom.com>
2017-08-03 16:55:42 +0200

    Removed insecure and autopublish. Added security and user filtering of todos.

--- todos.js
+++ todos.js
@@ -1,6 +1,8 @@
 Todos = new Mongo.Collection('todos');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('todos');
+
   // Template Helpers
   Template.main.helpers({
     todos: function() {
@@ -35,6 +37,17 @@ if (Meteor.isClient) {
   });
 }
 
+// Meteor Server
+if (Meteor.isServer) {
+  Meteor.publish('todos', function() {
+    if (!this.userId) {
+      return Todos.find();
+    } else {
+      return Todos.find({userId: this.userId});
+    }
+  });
+}
+
 // Meteor Methods
 Meteor.methods({
   addTodo: function(text) {
@@ -49,9 +62,17 @@ Meteor.methods({
     });
   },
   deleteTodo: function(todoId) {
+    var todo = Todos.findOne(todoId);
+    if (todo.userId !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
     Todos.remove(todoId);
   },
   setChecked: function(todoId, setChecked) {
+    var todo = Todos.findOne(todoId);
+    if (todo.userId !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
     Todos.update(todoId, {$set: {checked: setChecked}});
   }
 });
